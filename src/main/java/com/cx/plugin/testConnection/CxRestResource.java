72e0f9a9b158da8e6ea870d909ba820e876cd741
morad <morad.abuaql@checkmarx.com>
2020-07-08 18:12:34 +0300

    adding support for selfsigned

--- src/main/java/com/cx/plugin/testConnection/CxRestResource.java
+++ src/main/java/com/cx/plugin/testConnection/CxRestResource.java
@@ -4,13 +4,20 @@ package com.cx.plugin.testConnection;
 import com.cx.plugin.testConnection.dto.TestConnectionResponse;
 import com.cx.restclient.CxShragaClient;
 import com.cx.restclient.dto.Team;
+import com.cx.restclient.exception.CxClientException;
 import com.cx.restclient.sast.dto.Preset;
 import org.apache.commons.validator.routines.UrlValidator;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.codehaus.plexus.util.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.apache.http.conn.ssl.TrustAllStrategy;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -18,6 +25,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -49,7 +60,7 @@ public class CxRestResource {
         URL url;
         String urlToCheck;
         int statusCode = 400;
-
+        URLConnection urlConn;
         urlToCheck = StringUtils.defaultString(data.get("url"));
 
         try {
@@ -57,8 +68,14 @@ public class CxRestResource {
             if(!urlValidator.isValid(urlToCheck)){
                 return getInvalidUrlResponse(statusCode);
             }
+
             url = new URL(urlToCheck);
-            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            urlConn=url.openConnection();
+            if (url.getProtocol().equalsIgnoreCase("https")) {
+                ((HttpsURLConnection) urlConn).setSSLSocketFactory(getSSLSocketFactory());
+                ((HttpsURLConnection) urlConn).setHostnameVerifier(getHostnameVerifier());
+            }
+            //HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
 
             urlConn.connect();
         } catch (Exception e) {
@@ -101,7 +118,20 @@ public class CxRestResource {
         tcResponse = new TestConnectionResponse(result, null, null);
         return Response.status(statusCode).entity(tcResponse).build();
     }
+    private static SSLSocketFactory getSSLSocketFactory() throws CxClientException {
+        TrustStrategy acceptingTrustStrategy = new TrustAllStrategy();
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+            throw new CxClientException("Fail to set trust all certificate, 'SSLConnectionSocketFactory'", e);
+        }
+        return sslContext.getSocketFactory();
+    }
 
+    private static HostnameVerifier getHostnameVerifier() throws CxClientException {
+        return (hostname, session) -> true;
+    }
     @NotNull
     private TestConnectionResponse getTCFailedResponse() {
         presets = new ArrayList<Preset>() {{
@@ -116,7 +146,7 @@ public class CxRestResource {
 
     private boolean loginToServer(URL url, String username, String pd) {
         try {
-            shraga = new CxShragaClient(url.toString().trim(), username, pd, CX_ORIGIN, false, logger);
+            shraga = new CxShragaClient(url.toString().trim(), username, pd, CX_ORIGIN, true, logger);
             shraga.login();
 
             return true;
